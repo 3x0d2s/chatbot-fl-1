eea917a840be3119f110b50a85339dfec9cb5326
3x0d2s-msi <exodus.yarwood@icloud.com>
2021-10-20 16:59:28 +0300

    Добавил вариации продуктов для продажи

--- bot/config_pars.py
+++ bot/config_pars.py
@@ -9,7 +9,9 @@ def create_config(path_file):
     """Создаёт файл конфигурации."""
     config = configparser.ConfigParser()
     config.add_section("Settings")
-    config.set("Settings", "amount", "59000")
+    config.set("Settings", "amount_1", "39000")
+    config.set("Settings", "amount_2", "69000")
+    config.set("Settings", "amount_3", "129000")
     # Сохраняем конфиг. файл.
     with open(path_file, "w") as config_file:
         config.write(config_file)
@@ -22,17 +24,21 @@ def get_amount_config(path_file):
     config = configparser.ConfigParser()
     config.read(path_file)
     # Читаем значения из конфиг. файла.
-    return config.get("Settings", "amount")
+    return [
+        {"id": 1, "amount": config.get("Settings", "amount_1")},
+        {"id": 2, "amount": config.get("Settings", "amount_2")},
+        {"id": 3, "amount": config.get("Settings", "amount_3")},
+    ]
 
 
-def change_amount_config(path_file, value):
+def change_amount_config(path_file, product_id, value):
     """Сменяет текущий идентификатор недели."""
     if not os.path.exists(path_file):
         create_config(path_file)
     config = configparser.ConfigParser()
     config.read(path_file)
     # Меняем значения из конфиг. файла.
-    config.set("Settings", "amount", value)
+    config.set("Settings", f"amount_{product_id}", value)
     # Вносим изменения в конфиг. файл.
     with open(path_file, "w") as config_file:
         config.write(config_file)
--- bot/main.py
+++ bot/main.py
@@ -29,8 +29,9 @@ def createBD_FromDump(path_db, path_dump):
 
 
 def send_docs(user_id):
-    bot.send_document(user_id, data=config.DOC_1)
-    bot.send_document(user_id, data=config.DOC_2)
+    # bot.send_document(user_id, data=config.DOC_1)
+    # bot.send_document(user_id, data=config.DOC_2)
+    pass
 
 
 @bot.message_handler(commands=['start'])
@@ -40,21 +41,27 @@ def start_handler(message):
 
 @bot.message_handler(content_types=['text'])
 def change_amount(message):
-    msg = message.text.lower()
-    msg = msg.replace(' ', '')
+    msg = message.text.lower().replace(' ', '')
+
     user_id = message.from_user.id
-    new_amount = msg.replace('цена', '')
-    #
-    if user_id == config.ADMIN_ID and 'цена' in msg and new_amount != '':
+
+    product_id: str = msg[0]
+    new_amount: str = msg[1:].replace('цена', '')
+
+    if user_id == config.ADMIN_ID and 'цена' in msg and new_amount != '' and product_id.isnumeric() == True:
         value = new_amount + '00'
-        change_amount_config(config.PATH_SETTINGS, value)
-        bot.send_message(chat_id=user_id,
-                         text=f'Успешно! Цена теперь {new_amount} рублей.'
-                         )
-    elif user_id == config.ADMIN_ID and 'цена' in msg and new_amount == '':
-        bot.send_message(chat_id=user_id,
-                         text=f'Ошибка формата команды.'
-                         )
+
+        change_amount_config(config.PATH_SETTINGS, product_id, value)
+
+        bot.send_message(
+            chat_id=user_id,
+            text=f'Успешно! Цена теперь {new_amount} рублей.'
+        )
+    elif user_id == config.ADMIN_ID and 'цена' in msg:
+        bot.send_message(
+            chat_id=user_id,
+            text=f'Ошибка формата команды.'
+        )
 
 
 @bot.message_handler(content_types=['document'])
@@ -66,7 +73,7 @@ def start_text(user_id):
     msg = config.START_TEXT
     #
     pay_btn = InlineKeyboardButton(text='Оплатить',
-                                   callback_data='start_paymant'
+                                   callback_data='choose_product'
                                    )
     inline_markup = InlineKeyboardMarkup().add(pay_btn)
     #
@@ -78,18 +85,51 @@ def start_text(user_id):
     send_docs(user_id)
 
 
-@bot.callback_query_handler(func=lambda call: call.data == 'start_paymant')
+@bot.callback_query_handler(func=lambda call: call.data == 'choose_product')
 def start_payment(call):
     tg_id = call.message.chat.id
+    msg = 'Выберите вариант...'
+
+    amounts = get_amount_config(config.PATH_SETTINGS)
+
+    inline_markup = InlineKeyboardMarkup()
+    for product in config.PRODUCTS:
+        product_id = product["id"]
+        product_name = product["name"]
+        product_amount = amounts[product_id-1]["amount"][:-2]
+
+        product = InlineKeyboardButton(text=f"{product_name} - {product_amount} р.",
+                                       callback_data=f'product_{product["id"]}'
+                                       )
+        inline_markup.add(product)
+
+    bot.send_message(
+        chat_id=tg_id,
+        text=msg,
+        parse_mode='markdown',
+        reply_markup=inline_markup
+    )
+
+
+@bot.callback_query_handler(func=lambda call: call.data.startswith("product_"))
+def start_payment(call):
+    product_id = call.data[8:]
+
+    tg_id = call.message.chat.id
+
+    inline_markup = InlineKeyboardMarkup()
+    pay_btn_card = InlineKeyboardButton(
+        text='Картой',
+        callback_data=f'card_paymant_{product_id}'
+    )
+    pay_btn_qiwi = InlineKeyboardButton(
+        text='Qiwi',
+        callback_data=f'qiwi_paymant_{product_id}'
+    )
+    inline_markup.add(pay_btn_card)
+    inline_markup.add(pay_btn_qiwi)
+
     msg = 'Выберите способ оплаты...'
-    pay_btn_card = InlineKeyboardButton(text='Картой',
-                                        callback_data='card_paymant'
-                                        )
-    pay_btn_qiwi = InlineKeyboardButton(text='Qiwi',
-                                        callback_data='qiwi_paymant'
-                                        )
-    inline_markup = InlineKeyboardMarkup().add(pay_btn_card).add(pay_btn_qiwi)
-    #
     bot.send_message(chat_id=tg_id,
                      text=msg,
                      parse_mode='markdown',
@@ -97,59 +137,69 @@ def start_payment(call):
                      )
 
 
-@bot.callback_query_handler(func=lambda call: call.data == 'card_paymant')
+@bot.callback_query_handler(func=lambda call: call.data.startswith("card_paymant_"))
 def start_card_payment(call):
+    product_id = int(call.data[13:])
+
     tg_id = call.message.chat.id
-    amount = get_amount_config(config.PATH_SETTINGS)
-    #
+    amount = get_amount_config(config.PATH_SETTINGS)[product_id-1]["amount"]
+
     msg_id = call.message.message_id
     bot.delete_message(tg_id, msg_id)
-    #
+
     keyboard = InlineKeyboardMarkup()
-    keyboard.add(InlineKeyboardButton(text="Оплатить",
-                                      pay=True))
-    keyboard.add(InlineKeyboardButton(text="Отмена",
-                                      callback_data="cancel"))
-    #
-    bot.send_invoice(chat_id=tg_id,
-                     title='Доступ к сказкам',
-                     description=config.INVOICE_DESCRIPTION_TEXT,
-                     provider_token=config.PROVIDER_TOKEN,
-                     currency='RUB',
-                     photo_url=None,
-                     need_phone_number=False,
-                     need_email=False,
-                     is_flexible=False,
-                     prices=[LabeledPrice(label='Доступ к сказкам',
-                                          amount=int(amount))],
-                     start_parameter='start_parameter',
-                     invoice_payload='coupon',
-                     reply_markup=keyboard
-                     )
+    keyboard.add(InlineKeyboardButton(
+        text="Оплатить",
+        pay=True)
+    )
+    keyboard.add(InlineKeyboardButton(
+        text="Отмена",
+        callback_data=f'product_{product_id}')
+    )
+
+    bot.send_invoice(
+        chat_id=tg_id,
+        title='Доступ к сказкам',
+        description=config.INVOICE_DESCRIPTION_TEXT,
+        provider_token=config.PROVIDER_TOKEN,
+        currency='RUB',
+        photo_url=None,
+        need_phone_number=False,
+        need_email=False,
+        is_flexible=False,
+        prices=[LabeledPrice(label='Доступ к сказкам',
+                             amount=int(amount))],
+        start_parameter='start_parameter',
+        invoice_payload=str(product_id),
+        reply_markup=keyboard
+    )
 
 
 @bot.pre_checkout_query_handler(func=lambda query: True)
 def checkout(query):
-    bot.answer_pre_checkout_query(pre_checkout_query_id=query.id,
-                                  ok=True
-                                  )
+    bot.answer_pre_checkout_query(
+        pre_checkout_query_id=query.id,
+        ok=True
+    )
 
 
 @bot.message_handler(content_types=['successful_payment'])
 def got_payment(message):
     tg_id = message.chat.id
-    successful_payment(tg_id)
+    product_id = int(message.successful_payment.invoice_payload)
+    successful_payment(tg_id, product_id)
 
 
-@bot.callback_query_handler(func=lambda call: call.data == 'qiwi_paymant')
+@bot.callback_query_handler(func=lambda call: call.data.startswith("qiwi_paymant_"))
 def start_qiwi_payment(call):
+    product_id = int(call.data[13:])
+
     tg_id = call.message.chat.id
-    #
+
     msg_id = call.message.message_id
     bot.delete_message(tg_id, msg_id)
-    #
+
     random_code = randint(100000, 999999)
-    #
     db = paymentDirect('db/payment.db')
     if db.get_payment_code(tg_id) == None:
         db.add_payment_to_stack(tg_id, random_code)
@@ -157,53 +207,65 @@ def start_qiwi_payment(call):
         db.delete_payment(tg_id)
         db.add_payment_to_stack(tg_id, random_code)
     db.close()
-    #
-    btn_1 = InlineKeyboardButton(text='Проверить оплату',
-                                 callback_data='check_payment'
-                                 )
-    btn_2 = InlineKeyboardButton(text='Отмена',
-                                 callback_data='cancel'
-                                 )
-    inline_markup = InlineKeyboardMarkup().add(btn_1).add(btn_2)
-    #
-    amount = int(get_amount_config('config/settings.ini')) // 100
-    #
-    msg = f'Информация об оплате\n🥝 QIWI-кошелек: +{config.QIWI_ACCOUNT}\n📝 Комментарий к переводу: {random_code}\n💰 Сумма для оплаты: {amount} руб.\n*Внимание*\nЗаполняйте номер телефона и комментарий при переводе внимательно!\nПосле перевода нажмите кнопку *Проверить оплату*.\nОплачивая, Вы соглашаетесь с пользовательским соглашением и политикой конфиденциальности.'
-    bot.send_message(chat_id=tg_id,
-                     text=msg,
-                     parse_mode='markdown',
-                     reply_markup=inline_markup
-                     )
+
+    inline_markup = InlineKeyboardMarkup()
+    btn_1 = InlineKeyboardButton(
+        text='Проверить оплату',
+        callback_data=f'check_payment_{product_id}'
+    )
+    btn_2 = InlineKeyboardButton(
+        text='Отмена',
+        callback_data=f'product_{product_id}'
+    )
+    inline_markup.add(btn_1)
+    inline_markup.add(btn_2)
+
+    amount = get_amount_config(config.PATH_SETTINGS)[
+        product_id-1]["amount"][:-2]
+
+    msg = f'Информация об оплате\n🥝 QIWI-кошелек: +{config.QIWI_ACCOUNT}\n' \
+        f'📝 Комментарий к переводу: {random_code}\n💰 Сумма для оплаты: {amount} руб.\n' \
+        '*Внимание*\nЗаполняйте номер телефона и комментарий при переводе внимательно!\n' \
+        'После перевода нажмите кнопку *Проверить оплату*.\nОплачивая, Вы соглашаетесь с ' \
+        'пользовательским соглашением и политикой конфиденциальности.'
+    bot.send_message(
+        chat_id=tg_id,
+        text=msg,
+        parse_mode='markdown',
+        reply_markup=inline_markup
+    )
 
 
-@bot.callback_query_handler(func=lambda call: call.data == 'check_payment')
+@bot.callback_query_handler(func=lambda call: call.data.startswith("check_payment_"))
 def check_payment(call):
+    product_id = int(call.data[14:])
+
     db = paymentDirect('db/payment.db')
     user_id = call.from_user.id
-    #
     code = db.get_payment_code(user_id)[0]
-    #
+
     isBalanceReplenished = False
-    #
-    amount_true = int(get_amount_config(config.PATH_SETTINGS)) // 100
-    #
+
+    amount_true = get_amount_config(config.PATH_SETTINGS)[
+        product_id - 1]["amount"][:-2]
+
     req = get_qiwi_data()
-    #
     for i in req['data']:
         step_code = i['comment']
         amount = i['sum']['amount']
         if step_code == code and amount == amount_true:
             isBalanceReplenished = True
             break
-    #
+
     if isBalanceReplenished == True:
         db.delete_payment(user_id)
-        successful_payment(user_id)
+        successful_payment(user_id, product_id)
     else:
-        bot.send_message(chat_id=user_id,
-                         text="*Ошибка:* платежа не обнаружено.",
-                         parse_mode="markdown",
-                         )
+        bot.send_message(
+            chat_id=user_id,
+            text="*Ошибка:* платежа не обнаружено.",
+            parse_mode="markdown",
+        )
 
 
 def get_qiwi_data():
@@ -217,38 +279,17 @@ def get_qiwi_data():
     return json.loads(h)
 
 
-@bot.callback_query_handler(func=lambda call: call.data == 'cancel')
-def cancel_payment(call):
-    tg_id = call.message.chat.id
-    msg_id = call.message.message_id
-    bot.delete_message(chat_id=tg_id, message_id=msg_id)
-    #
-    db = paymentDirect('db/payment.db')
-    db.delete_payment(tg_id)
-    db.close()
-    #
-    pay_btn_card = InlineKeyboardButton(text='Картой',
-                                        callback_data='card_paymant'
-                                        )
-    pay_btn_qiwi = InlineKeyboardButton(text='Qiwi',
-                                        callback_data='qiwi_paymant'
-                                        )
-    inline_markup = InlineKeyboardMarkup().add(pay_btn_card).add(pay_btn_qiwi)
-    #
-    msg = 'Выберите способ оплаты...'
-    bot.send_message(chat_id=tg_id, text=msg, reply_markup=inline_markup)
-
-
-def successful_payment(user_id):
+def successful_payment(user_id, product_id: int):
     url_btn = InlineKeyboardButton(text='Перейти в канал...',
-                                   url=config.PRIVATE_URL
+                                   url=config.PRODUCTS[product_id-1]["url"]
                                    )
     inline_markup = InlineKeyboardMarkup().add(url_btn)
     #
-    bot.send_message(chat_id=user_id,
-                     text='Платёж прошёл, спасибо за оплату!',
-                     reply_markup=inline_markup
-                     )
+    bot.send_message(
+        chat_id=user_id,
+        text='✅ Платёж прошёл, спасибо за оплату!',
+        reply_markup=inline_markup
+    )
 
 
 # RUN
